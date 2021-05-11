24bec62b03cb2e88faad44507a55e906118f7c38
faisalcep <faisalcep@yahoo.com>
2021-05-11 16:50:29 -0400

    firstCommit

---
+++ app.js
@@ -0,0 +1,164 @@
+const playerScore = document.querySelector(".player-score p");
+const computerScore = document.querySelector(".computer-score p");
+let pScore= localStorage.getItem('playerScore') ? localStorage.getItem('playerScore') : 0
+let cScore=localStorage.getItem('computerScore') ? localStorage.getItem('computerScore') : 0
+playerScore.textContent = pScore;
+computerScore.textContent = cScore;
+
+const game = () => {
+  //Start the Game
+  const startGame = () => {
+    const playBtn = document.querySelector(".intro button");
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector(".match");
+
+    playBtn.addEventListener("click", () => {
+      introScreen.classList.add("fadeOut");
+      match.classList.add("fadeIn");
+    });
+  };
+  //Play Match
+  const playMatch = () => {
+    const options = document.querySelectorAll(".options button");
+    const playerHand = document.querySelector(".player-hand");
+    const computerHand = document.querySelector(".computer-hand");
+    const hands = document.querySelectorAll(".hands img");
+
+    hands.forEach(hand => {
+      hand.addEventListener("animationend", function() {
+        this.style.animation = "";
+      });
+    });
+    //Computer Options
+    const computerOptions = ["rock", "paper", "scissors"];
+
+    options.forEach(option => {
+      option.addEventListener("click", function() {
+        //Computer Choice
+        const computerNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerNumber];
+
+        setTimeout(() => {
+          //Here is where we call compare hands
+          compareHands(this.textContent, computerChoice);
+          //Update Images
+          playerHand.src = `./assets/${this.textContent}.png`;
+          computerHand.src = `./assets/${computerChoice}.png`;
+        }, 2000);
+        //Animation
+        playerHand.style.animation = "shakePlayer 2s ease";
+        computerHand.style.animation = "shakeComputer 2s ease";
+      });
+    });
+  };
+
+  const updateScore = () => {
+    localStorage.setItem('playerScore', pScore);
+    localStorage.setItem('computerScore', cScore);
+    showScore();
+  };
+
+  const showScore =()=>{
+    const playerScore = document.querySelector(".player-score p");
+    const computerScore = document.querySelector(".computer-score p");
+    
+    cScore=localStorage.getItem('computerScore');
+    pScore= localStorage.getItem('playerScore');
+    console.log(pScore,cScore );
+    playerScore.textContent = pScore;
+    computerScore.textContent = cScore;
+  };
+
+  const compareHands = (playerChoice, computerChoice) => {
+    //Update Text
+    const winner = document.querySelector(".winner");
+    //Checking for a tie
+    if (playerChoice === computerChoice) {
+      winner.textContent = "It is a tie";
+      return;
+    }
+    //Check for Rock
+    if (playerChoice === "rock") {
+      if (computerChoice === "scissors") {
+        winner.textContent = "Player Wins";
+        pScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Computer Wins";
+        cScore++;
+        updateScore();
+        return;
+      }
+    }
+    //Check for Paper
+    if (playerChoice === "paper") {
+      if (computerChoice === "scissors") {
+        winner.textContent = "Computer Wins";
+        cScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Player Wins";
+        pScore++;
+        updateScore();
+        return;
+      }
+    }
+    //Check for Scissors
+    if (playerChoice === "scissors") {
+      if (computerChoice === "rock") {
+        winner.textContent = "Computer Wins";
+        cScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Player Wins";
+        pScore++;
+        updateScore();
+        return;
+      }
+    }
+  };
+
+  //Is call all the inner function
+  startGame();
+  playMatch();
+};
+
+//start the game function
+game();
+
+// Reset
+const resetGame = document.querySelector('#reset-game');
+resetGame.addEventListener('click', function () {
+  localStorage.clear();
+  location.reload();
+});
+
+
+// Modal
+const modalTriggers = document.querySelectorAll('.popup-trigger')
+const modalCloseTrigger = document.querySelector('.popup-modal__close')
+const bodyBlackout = document.querySelector('.body-blackout')
+
+modalTriggers.forEach(trigger => {
+  trigger.addEventListener('click', () => {
+    const { popupTrigger } = trigger.dataset
+    const popupModal = document.querySelector(`[data-popup-modal="${popupTrigger}"]`)
+
+    popupModal.classList.add('is--visible')
+    bodyBlackout.classList.add('is-blacked-out')
+    
+    popupModal.querySelector('.popup-modal__close').addEventListener('click', () => {
+       popupModal.classList.remove('is--visible')
+       bodyBlackout.classList.remove('is-blacked-out')
+    })
+    
+    bodyBlackout.addEventListener('click', () => {
+      // TODO: Turn into a function to close modal
+      popupModal.classList.remove('is--visible')
+      bodyBlackout.classList.remove('is-blacked-out')
+    })
+  })
+})
